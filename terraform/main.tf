eb7254b8677472be491f8196496009dff18c72e6
Shrikant Nadgauda <shri.gaudaa@gmail.com>
2026-08-19 09:12:29 +0530

    added parent child module and write code for Rgs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,35 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "5.1.0"
-    }
-  }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "5.1.0"
+#     }
+#   }
+# }
+
+
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = "rg1"
+#   location = "East US"
+# }
+
+# resource "azurerm_resource_group" "rg2" {
+#   name = "rg2"
+#   location = "Central India"
+# }
+
+# resource "azurerm_resource_group" "rg3" {
+#   name = "rg3"
+#   location = " West Europe"
+# }
+
+
+
+module "resource_groups" {
+
+  source = "./modules/resource-group"
+
+  resource_groups = var.resource_groups
+
 }
-
-
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg1"
-  location = "East US"
-}
-
-resource "azurerm_resource_group" "rg2" {
-  name = "rg2"
-  location = "Central India"
-}
-
-resource "azurerm_resource_group" "rg3" {
-  name = "rg3"
-  location = " West Europe"
-}
-
-
-
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_groups" {
+
+  description = "Resource Group configuration"
+
+  type = map(object({
+    name     = string
+    location = string
+  }))
+
+}
